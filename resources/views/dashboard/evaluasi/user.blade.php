e3c66dbb1eebb390db0a3cf57cf559bb70da1c8e
aphynt <ahmadfadillah502@gmail.com>
2026-02-23 16:05:41 +0800

    update

--- resources/views/dashboard/evaluasi/user.blade.php
+++ resources/views/dashboard/evaluasi/user.blade.php
@@ -111,9 +111,7 @@
 
                     <div class="col-md-12">
                         <label class="form-label">Keterangan</label>
-                        <textarea class="form-control" rows="5" readonly>
-                            {{ $data->keterangan }}
-                        </textarea>
+                        <textarea class="form-control" rows="5" readonly>{{ $data->keterangan }}</textarea>
                     </div>
 
                     <div class="col-md-12">
@@ -138,7 +136,6 @@
                             <div class="alert svg-primary alert-primary alert-dismissible fade show custom-alert-icon shadow-sm" role="alert">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 0 24 24" width="1.5rem" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
                                     {{ $data->keterangan_evaluasi }}
-                                </div>
                             </div>
                         @endif
                     </div>
